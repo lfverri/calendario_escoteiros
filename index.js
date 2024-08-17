53d37993178799ab2ef3c70a8718ec48b1ebd654
Lucas Verri <lfverri@sga.pucminas.br>
2024-08-17 10:23:41 -0300

    :sparkles: commit inicial

---
+++ index.js
@@ -0,0 +1,42 @@
+// Seleciona o botão pelo class name
+const button1 = document.querySelector('.button1');
+
+// Adiciona o evento de clique ao botão
+button1.addEventListener('click', function () {
+    // Redireciona para a página equipe1.html dentro da pasta Equipe1
+    window.location.href = 'Equipe1/equipe1.html';
+});
+
+
+const button2 = document.querySelector('.button2');
+
+// Adiciona o evento de clique ao botão
+button2.addEventListener('click', function () {
+    // Redireciona para a página equipe1.html dentro da pasta Equipe1
+    window.location.href = 'Equipe2/equipe2.html';
+});
+
+const button3 = document.querySelector('.button3');
+
+// Adiciona o evento de clique ao botão
+button3.addEventListener('click', function () {
+    // Redireciona para a página equipe1.html dentro da pasta Equipe1
+    window.location.href = 'Equipe3/equipe3.html';
+});
+
+const button4 = document.querySelector('.button4');
+
+// Adiciona o evento de clique ao botão
+button4.addEventListener('click', function () {
+    // Redireciona para a página equipe1.html dentro da pasta Equipe1
+    window.location.href = 'Equipe4/equipe4.html';
+});
+
+const button5 = document.querySelector('.button5');
+
+// Adiciona o evento de clique ao botão
+button5.addEventListener('click', function () {
+    // Redireciona para a página equipe1.html dentro da pasta Equipe1
+    window.location.href = 'Equipe5/equipe5.html';
+});
+
